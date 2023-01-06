0bf4fcc7fe989b309aa648af0c9b7ce67c09103f
Jagi <ba87688@gmail.com>
2023-01-06 08:27:53 -0500

    Navigation update

--- app/src/main/java/com/example/jetpackcompose/ui/screens/electionscreen/ElectionData.kt
+++ app/src/main/java/com/example/jetpackcompose/ui/screens/electionscreen/ElectionData.kt
@@ -1,9 +1,13 @@
 package com.example.jetpackcompose.ui.screens.electionscreen
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 
 @Composable
 fun ElectionData( navController: NavHostController){
+
+    
+    Text(text = "Next screen")
 
 }
--- app/src/main/java/com/example/jetpackcompose/ui/screens/mainscreen/MainScreen.kt
+++ app/src/main/java/com/example/jetpackcompose/ui/screens/mainscreen/MainScreen.kt
@@ -1,10 +1,27 @@
 package com.example.jetpackcompose.ui.screens.mainscreen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import com.example.jetpackcompose.navigation.Screen
 
 @Composable
 fun MainScreen( navController: NavHostController){
-Text(text = "Rocky")
+
+
+    Column() {
+
+        Text(text = "Rocky")
+
+        Button(onClick = {
+            navController.navigate(route = Screen.ElectionData.route)
+        }) {
+            Text(text = "Go to next Screen")
+
+        }
+
+    }
+
 }
